e4cb133d7a6d98f7ec1289d25db6e51db759724a
Alex Liu <avliu@buffalo.edu>
2019-06-29 21:10:58 -0400

    Baseline model yields 10% MAPE, beginning of text processing

--- Business Analytics/preprocess.py
+++ Business Analytics/preprocess.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from fastai.tabular import add_datepart
+import re
 
+
+# 1. add dateparts
 
 # df_stats = pd.read_csv('training_set_stats.csv')
 #
@@ -11,25 +14,74 @@ from fastai.tabular import add_datepart
 # add_datepart(df=df_stats_datetime, field_name='Created', time=True)
 # df_stats_datetime.to_csv('training_set_datetimes.csv')
 
-df_stats_datetime = pd.read_csv('training_set_datetimes.csv')
-df_stats_datetime = df_stats_datetime.drop(columns=['Unnamed: 0', 'CreatedMonth', 'CreatedDay', 'CreatedWeek',
-                                                    'CreatedIs_month_end', 'CreatedIs_month_start',
-                                                    'CreatedIs_quarter_end', 'CreatedIs_quarter_start',
-                                                    'CreatedIs_year_start', 'CreatedIs_year_end',
-                                                    'CreatedSecond', 'CreatedElapsed', 'CreatedYear'])
-df_stats_datetime['CreatedTotalMinutes'] = df_stats_datetime.loc[:, 'CreatedHour']*60 +\
-                                           df_stats_datetime.loc[:,'CreatedMinute']
-df_stats_datetime = df_stats_datetime.drop(columns=['CreatedHour', 'CreatedMinute'])
-df_stats_datetime['CreatedWeekend'] = (df_stats_datetime.loc[:,'CreatedDayofweek']).apply(lambda x: (1 if x > 3 else 0))
-df_stats_datetime = df_stats_datetime.drop(columns=['CreatedDayofweek'])
 
-df_stats_normalized = df_stats_datetime
-df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')] = \
-    (df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')] -
-     df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')].mean()) / \
-    (df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')].max() -
-     df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')].min())
+# 2. drop irrelevant columns, consolidate hours and minutes, create training_set_final_0.csv
 
-df_stats_normalized.to_csv('training_set_final_0.csv', index=False)
+# df_stats_datetime = pd.read_csv('training_set_datetimes.csv')
+# df_stats_datetime = df_stats_datetime.drop(columns=['Unnamed: 0', 'CreatedMonth', 'CreatedDay', 'CreatedWeek',
+#                                                     'CreatedIs_month_end', 'CreatedIs_month_start',
+#                                                     'CreatedIs_quarter_end', 'CreatedIs_quarter_start',
+#                                                     'CreatedIs_year_start', 'CreatedIs_year_end',
+#                                                     'CreatedSecond', 'CreatedElapsed', 'CreatedYear'])
+# df_stats_datetime['CreatedTotalMinutes'] = df_stats_datetime.loc[:, 'CreatedHour']*60 +\
+#                                            df_stats_datetime.loc[:,'CreatedMinute']
+# df_stats_datetime = df_stats_datetime.drop(columns=['CreatedHour', 'CreatedMinute'])
+# # df_stats_datetime['CreatedWeekend'] = (df_stats_datetime.loc[:,'CreatedDayofweek']).apply(lambda x: (1 if x > 3 else 0))
+# # df_stats_datetime = df_stats_datetime.drop(columns=['CreatedDayofweek'])
+#
+# df_stats_normalized = df_stats_datetime
+# df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')] = \
+#     (df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')] -
+#      df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')].mean()) / \
+#     (df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')].max() -
+#      df_stats_normalized.loc[:,('Followers at Posting','CreatedDayofyear','CreatedTotalMinutes')].min())
+#
+# df_stats_normalized.to_csv('training_set_final_0.csv', index=False)
 
-f=5
+
+# 3. prepare text data- map scraped instagram usernames to actual usernames
+#    WRONG USERNAMES (changed only in training_set_same.csv and holdout_set_same.csv)
+#       all hawks -> atlhawks
+#       okc thunder-> okcthunder
+#       breaks@out -> breaks out
+#    WRONG USERNAMES (changed only in instagram_usernames.csv):
+#       original00g -> youngheirgordon
+#       bobimarjanovic13 -> boban
+#       enikonhart -> enikohart
+#       planetpat5 -> patconnaughton
+#       steph -> stephencurry30
+#       unclejg8 -> unclejeffgreen
+#       frenchtouch20 -> 7tlc
+#       shaigilalex -> shai
+#       mitchellness -> mitchellandness
+#       the2kferguson -> fergski23
+#       kuzmakyle -> kuz
+#       1jordanbell -> jbell
+#       iisaiahthomas -> isaiahthomas
+#       furkankorkkmaz -> furkankorkmaz
+#       gdhayward -> gordonhayward
+#    filled out account type- 1=Player (current/near current), 2=Team, 3=Celebrity, 4=Company/Organization, 5=Other,
+
+df_train = pd.read_csv('text_processing/training_set_same.csv')
+df_holdout = pd.read_csv('text_processing/holdout_set_same.csv')
+
+df_train = df_train.loc[:,'Description']
+df_holdout = df_holdout.loc[:,'Description']
+
+users = set()
+
+for x in df_train:
+    if x is not None and type(x) == str:
+        users.update(set(re.findall('@[A-Za-z0-9_.]+', x)))
+
+for x in df_holdout:
+    if x is not None and type(x) == str:
+        users.update(set(re.findall('@[A-Za-z0-9_.]+', x)))
+
+file = open('text_processing/instagram_usernames.csv', 'w')
+
+file.write('scraped username,actual username,type\n')
+
+for user in users:
+    file.write(f'{user[1:]},{user[1:]},\n')
+
--- Business Analytics/process.py
+++ Business Analytics/process.py
@@ -6,12 +6,12 @@ import numpy as np
 
 def build_model(input_size):
     input = keras.Input(shape=(input_size,))
-    x = keras.layers.Dense(10, activation='sigmoid')(input)
-    x = keras.layers.Dropout(.5)(x)
-    x = keras.layers.Dense(1, activation='relu')(x)
+    x = keras.layers.Dense(32, activation='elu')(input)
+    x = keras.layers.Dense(32, activation='elu')(x)
+    x = keras.layers.Dense(1)(x)
 
-    model = keras.Model(inputs=[input], outputs=[x])
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['mape'])
+    model = keras.Model(inputs=input, outputs=x)
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
 
     return model
 
@@ -22,7 +22,7 @@ train_labels = train.iloc[:, 0].to_numpy()
 
 num_epochs = 20
 all_scores = []
-k = 50
+k = 5
 num_val_samples = train_features.shape[0] // k
 
 for i in range(0, k):
@@ -34,9 +34,11 @@ for i in range(0, k):
                                          train_features[(i+1) * num_val_samples:]], axis=0)
     partial_train_targets = np.concatenate([train_labels[:i * num_val_samples],
                                          train_labels[(i+1) * num_val_samples:]], axis=0)
+    early_stop = keras.callbacks.EarlyStopping(monitor='mean_absolute_error', patience=10)
     model = build_model(train_features.shape[-1])
-    model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=1)
-    val_mape = model.evaluate(val_data, val_targets)
+    model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=1, callbacks=[early_stop])
+    val_loss, val_mape = model.evaluate(val_data, val_targets)
     all_scores.append(val_mape)
 
-a = 3
+print(all_scores)
+print(np.mean(all_scores))
